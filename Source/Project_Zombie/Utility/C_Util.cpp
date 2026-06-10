9aafe19d21d3f9086408f07bb57bf3e4680eda16
DongHyun96 <wadanka09@gmail.com>
2026-06-10 19:31:27 +0900

    HUD 구현 중 / Utility Print 함수 추가

---
+++ Source/Project_Zombie/Utility/C_Util.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Util.h"
+
+void UC_Util::Print(const FString& str, const FColor& InColor, float TimeToDisplay)
+{
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(int data, const FColor& InColor, float TimeToDisplay)
+{
+	FString str = FString::FromInt(data);
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(float data, const FColor& InColor, float TimeToDisplay)
+{
+	FString str = FString::SanitizeFloat(data);
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(double data, const FColor& InColor, float TimeToDisplay)
+{
+	FString str = FString::SanitizeFloat(data);
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(const FVector& data, const FColor& InColor, float TimeToDisplay)
+{
+	FString str = data.ToString();
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(const FVector2D& data, const FColor& InColor, float TimeToDisplay)
+{
+	FString str = data.ToString();
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *str);
+}
+
+void UC_Util::Print(const FTransform& data, const FColor& InColor, float TimeToDisplay)
+{
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *data.ToString());
+}
+
+void UC_Util::Print(const FRotator& data, const FColor& InColor, float TimeToDisplay)
+{
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, *data.ToString());
+}
+
+void UC_Util::Print(AActor* Actor, const FColor& InColor, float TimeToDisplay)
+{
+	FString AddressString = FString::Printf(TEXT("%p"), Actor);
+	GEngine->AddOnScreenDebugMessage(-1, TimeToDisplay, InColor, AddressString);
+}
+
+void UC_Util::PrintLogMessage(const FString& str)
+{
+	UE_LOG(LogTemp, Log, TEXT("&s"), *str);
+}
---
+++ Source/Project_Zombie/Utility/C_Util.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "C_Util.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ZOMBIE_API UC_Util : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+
+	static void Print(const FString&		str,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(int					data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(float					data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(double				data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(const FVector&		data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(const FVector2D&		data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(const FTransform&		data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(const FRotator&		data,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+	static void Print(AActor*				Actor,	const FColor& InColor = FColor::Red, float TimeToDisplay = 1.f);
+
+	static void PrintLogMessage(const FString& str);
+
+	static void ClearAllMessages() { GEngine->ClearOnScreenDebugMessages(); }
+
+public:
+	/// <summary>
+	/// FVector XY 성분 떼어서 FVector2D return
+	/// </summary>
+	static FVector2D GetXY(const FVector& InVector) { return FVector2D(InVector.X, InVector.Y); }
+
+};
