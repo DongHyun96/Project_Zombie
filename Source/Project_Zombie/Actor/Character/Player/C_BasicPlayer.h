8f9120622cc3690af65a5e29f4d3ab5295312714
shb03008 <sshb03008@gmail.com>
2026-06-10 11:39:02 +0900

    클래스 기본 생성

---
+++ Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/Character/C_BasicCharacter.h"
+#include "C_BasicPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ZOMBIE_API AC_BasicPlayer : public AC_BasicCharacter
+{
+	GENERATED_BODY()
+	
+};
