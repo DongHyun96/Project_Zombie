f9bfdaff2a241758f27599e26af57e08a62b06f7
Hhyunseung <105931128+Hhyunseung@users.noreply.github.com>
2026-06-10 15:56:12 +0900

    InputAction 들 선언

--- Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.cpp
+++ Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.cpp
@@ -3,3 +3,80 @@
 
 #include "Actor/Character/Player/C_BasicPlayer.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
+#include "EnhancedActionKeyMapping.h"
+#include "EnhancedInputSubsystems.h"
+
+#include "C_BasicPlayer.h"
+
+
+void AC_BasicPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AC_BasicPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AC_BasicPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* pEIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_Move)
+			pEIC->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AC_BasicPlayer::MoveAction);
+		
+		if (IA_Look)
+			pEIC->BindAction(IA_Look, ETriggerEvent::Triggered, this, &AC_BasicPlayer::LookAction);
+		
+		if (IA_Jump)
+			pEIC->BindAction(IA_Jump, ETriggerEvent::Started, this, &AC_BasicPlayer::JumpAction);
+		
+		if (IA_Fire)
+			pEIC->BindAction(IA_Fire, ETriggerEvent::Started, this, &AC_BasicPlayer::FireAction);
+	}
+
+
+}
+void AC_BasicPlayer::MoveAction(const FInputActionValue& Value)
+{
+	if (GetController() != nullptr)
+	{
+		FVector2D Input = Value.Get<FVector2D>();
+
+		FVector vF = GetActorForwardVector();
+		FVector vR = GetActorRightVector();
+
+		AddMovementInput(vF, Input.X);
+		AddMovementInput(vR, Input.Y);
+	}
+}
+
+void AC_BasicPlayer::LookAction(const FInputActionValue& Value)
+{
+	if (GetController() != nullptr)
+	{
+		FVector2D Input = Value.Get<FVector2D>();
+
+		AddControllerYawInput(Input.X);
+		AddControllerPitchInput(Input.Y);
+	}
+}
+
+void AC_BasicPlayer::JumpAction()
+{
+	Super::Jump();
+}
+
+void AC_BasicPlayer::FireAction()
+{
+	// 무기 컴포넌트에서 발사 함수 호출
+}
