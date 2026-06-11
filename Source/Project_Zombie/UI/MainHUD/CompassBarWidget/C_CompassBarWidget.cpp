f35d852bc46aba552278c81a949f21e15eaaf268
DongHyun96 <wadanka09@gmail.com>
2026-06-11 13:59:13 +0900

    etc.
    Syntactic sugar

--- Source/Project_Zombie/UI/MainHUD/CompassBarWidget/C_CompassBarWidget.cpp
+++ Source/Project_Zombie/UI/MainHUD/CompassBarWidget/C_CompassBarWidget.cpp
@@ -40,65 +40,7 @@ void UC_CompassBarWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaT
 
 void UC_CompassBarWidget::SetDisplayDegreeText(float _RotationYaw)
 {
-	/*const float Yaw = (_RotationYaw < 0.f) ? _RotationYaw + 360.f : _RotationYaw;
-	int32 DegreeValue{};
-	
-	if (Yaw >= 357.5) DegreeValue = 0;
-	else
-	{
-		const int32 YawInt  = Yaw + 2.5f;
-		const float ToFloat = YawInt / 5.f;
-		DegreeValue         = ToFloat * 5;
-	}
-	
-	if (m_DegreeToAlphabet.Contains(DegreeValue))
-	{
-		DegIndicatorText->SetText(m_DegreeToAlphabet[DegreeValue]);
-		DegIndicatorText->SetColorAndOpacity
-		(
-			DegreeValue == 0 ? FSlateColor(m_ZeroDegColor) : FSlateColor(FLinearColor::White) 
-		);
-	}
-	else
-	{
-		DegIndicatorText->SetText(FText::AsNumber(DegreeValue));
-		DegIndicatorText->SetColorAndOpacity(FSlateColor(FLinearColor::White));
-	}*/
-	
-	/*// -180~180 -> 0~360 변환
-	const float Yaw = (_RotationYaw < 0.f)
-		? _RotationYaw + 360.f
-		: _RotationYaw;
-
-	// 가장 가까운 5도 단위로 반올림
-	int32 DegreeValue = FMath::RoundToInt(Yaw / 5.f) * 5;
-
-	// 360도는 0도로 처리
-	if (DegreeValue >= 360)
-	{
-		DegreeValue = 0;
-	}
-
-	if (const FText* DirectionText = m_DegreeToAlphabet.Find(DegreeValue))
-	{
-		DegIndicatorText->SetText(*DirectionText);
-
-		DegIndicatorText->SetColorAndOpacity(
-			DegreeValue == 0
-				? FSlateColor(m_ZeroDegColor)
-				: FSlateColor(FLinearColor::White)
-		);
-	}
-	else
-	{
-		DegIndicatorText->SetText(FText::AsNumber(DegreeValue));
-		DegIndicatorText->SetColorAndOpacity(
-			FSlateColor(FLinearColor::White)
-		);
-	}*/
-	
-	const int32 DegreeValue = FMath::RoundToInt(FRotator::ClampAxis(_RotationYaw) / 5.f) * 5 % 360;
-
+	const int32 DegreeValue    = FMath::RoundToInt(FRotator::ClampAxis(_RotationYaw) / 5.f) * 5 % 360;
 	const FText* DirectionText = m_DegreeToAlphabet.Find(DegreeValue);
 
 	DegIndicatorText->SetText
