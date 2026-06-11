e1eb3a81409815534f52fbd4218fbe9e25185c0f
shb03008 <sshb03008@gmail.com>
2026-06-11 12:47:29 +0900

    Create InputComponent
    C_BasicPlayer에서 작성하던 Enhanced Input System을
    별도의 컴포넌트화 시킴.

--- Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.h
+++ Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.h
@@ -40,6 +40,10 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (DisplayName = "MainCamera"))
 	class UCameraComponent* m_Camera;
 
+	// 새로 추가된 우리만의 커스텀 인풋 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (DisplayName = "PlayerInput"))
+	class UC_BasicPlayerInputComponent* m_InputComponent;
+
 // 캐릭터 상태 // 이건 나중에 BasicCharacter 로 옮겨도?
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
@@ -50,57 +54,57 @@ protected:
 
 // InputContainer 를 통해서 입력을 받을지...
 // Input
-protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputMappingContext* DefaultMappingContext;
-
-	// 이동			// WASD
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Move;
-
-	// 시점 회전		// Mouse
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Look;
-
-	// 점프			// SpaceBar
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Jump;
-
-	// 공격			// LMB
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Fire;
-
-	// 시점 전환		// RMB
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Aim;
-
-	// 재장전		// R
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Reload;
-
-	// 주무기 1 장착	// 1
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_EquipPrimary1;
-
-	// 주무기 2 장착	// 2
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_EquipPrimary2;
-
-	// 보조무기 장착	// 3
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_EquipMelee;
-
-	// 투척류 무기 장착 // 4
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_EquipThrowable;
-
-	// 인벤토리 열기	// Tab
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_ToggleInventory;
-
-	// 상호작용		// E
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	UInputAction* IA_Interact;
+//protected:
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputMappingContext* DefaultMappingContext;
+//
+//	// 이동			// WASD
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Move;
+//
+//	// 시점 회전		// Mouse
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Look;
+//
+//	// 점프			// SpaceBar
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Jump;
+//
+//	// 공격			// LMB
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Fire;
+//
+//	// 시점 전환		// RMB
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Aim;
+//
+//	// 재장전		// R
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Reload;
+//
+//	// 주무기 1 장착	// 1
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_EquipPrimary1;
+//
+//	// 주무기 2 장착	// 2
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_EquipPrimary2;
+//
+//	// 보조무기 장착	// 3
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_EquipMelee;
+//
+//	// 투척류 무기 장착 // 4
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_EquipThrowable;
+//
+//	// 인벤토리 열기	// Tab
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_ToggleInventory;
+//
+//	// 상호작용		// E
+//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+//	UInputAction* IA_Interact;
 
 	// 인칭 전환
 	//UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
@@ -111,13 +115,13 @@ public:
 	EPlayerState GetPlayerState() { return m_PlayerState; }
 
 // Input Action Functions
-private:
-	void InitInput();
-protected:
-	void MoveAction(const FInputActionValue& Value);
-	void LookAction(const FInputActionValue& Value);
-	void JumpAction();
-	void FireAction();
+//private:
+//	void InitInput();
+//protected:
+//	void MoveAction(const FInputActionValue& Value);
+//	void LookAction(const FInputActionValue& Value);
+//	void JumpAction();
+//	void FireAction();
 
 
 protected:
@@ -126,6 +130,7 @@ protected:
 public:
 	virtual void Tick(float DeltaTime) override;
 
+	// Cotroller가 빙의할 때 실행되는 함수.
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 public:
