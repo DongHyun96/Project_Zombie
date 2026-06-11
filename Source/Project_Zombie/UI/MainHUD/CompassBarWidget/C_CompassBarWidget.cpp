4b5549da31b356a1add64c8493db74b1df33d21c
DongHyun96 <wadanka09@gmail.com>
2026-06-11 12:56:06 +0900

    HUD 상단 CompassBar 기능 추가
    1. Compassbar 기능 추가
    
    2. 폰트 추가
    
    3. 기타 UI용 이미지 추가

---
+++ Source/Project_Zombie/UI/MainHUD/CompassBarWidget/C_CompassBarWidget.cpp
@@ -0,0 +1,113 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_CompassBarWidget.h"
+
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Utility/C_Util.h"
+
+void UC_CompassBarWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UC_CompassBarWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (!CompassBarImage)
+	{
+		UC_Util::Print("From UC_CompassBarWidget::NativeConstruct : CompassBarImage not bound", FColor::Red, 10.f);
+		return;
+	}
+	
+	m_CompassBarDynamicMtrl = CompassBarImage->GetDynamicMaterial();
+}
+
+void UC_CompassBarWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (!PlayerController) return;
+
+	const float PCYaw = PlayerController->GetControlRotation().Yaw;
+	
+	m_CompassBarDynamicMtrl->SetScalarParameterValue(TEXT("yaw"), PCYaw / 360.f);
+	SetDisplayDegreeText(PCYaw);
+}
+
+void UC_CompassBarWidget::SetDisplayDegreeText(float _RotationYaw)
+{
+	/*const float Yaw = (_RotationYaw < 0.f) ? _RotationYaw + 360.f : _RotationYaw;
+	int32 DegreeValue{};
+	
+	if (Yaw >= 357.5) DegreeValue = 0;
+	else
+	{
+		const int32 YawInt  = Yaw + 2.5f;
+		const float ToFloat = YawInt / 5.f;
+		DegreeValue         = ToFloat * 5;
+	}
+	
+	if (m_DegreeToAlphabet.Contains(DegreeValue))
+	{
+		DegIndicatorText->SetText(m_DegreeToAlphabet[DegreeValue]);
+		DegIndicatorText->SetColorAndOpacity
+		(
+			DegreeValue == 0 ? FSlateColor(m_ZeroDegColor) : FSlateColor(FLinearColor::White) 
+		);
+	}
+	else
+	{
+		DegIndicatorText->SetText(FText::AsNumber(DegreeValue));
+		DegIndicatorText->SetColorAndOpacity(FSlateColor(FLinearColor::White));
+	}*/
+	
+	/*// -180~180 -> 0~360 변환
+	const float Yaw = (_RotationYaw < 0.f)
+		? _RotationYaw + 360.f
+		: _RotationYaw;
+
+	// 가장 가까운 5도 단위로 반올림
+	int32 DegreeValue = FMath::RoundToInt(Yaw / 5.f) * 5;
+
+	// 360도는 0도로 처리
+	if (DegreeValue >= 360)
+	{
+		DegreeValue = 0;
+	}
+
+	if (const FText* DirectionText = m_DegreeToAlphabet.Find(DegreeValue))
+	{
+		DegIndicatorText->SetText(*DirectionText);
+
+		DegIndicatorText->SetColorAndOpacity(
+			DegreeValue == 0
+				? FSlateColor(m_ZeroDegColor)
+				: FSlateColor(FLinearColor::White)
+		);
+	}
+	else
+	{
+		DegIndicatorText->SetText(FText::AsNumber(DegreeValue));
+		DegIndicatorText->SetColorAndOpacity(
+			FSlateColor(FLinearColor::White)
+		);
+	}*/
+	
+	const int32 DegreeValue = FMath::RoundToInt(FRotator::ClampAxis(_RotationYaw) / 5.f) * 5 % 360;
+
+	const FText* DirectionText = m_DegreeToAlphabet.Find(DegreeValue);
+
+	DegIndicatorText->SetText
+	(
+		DirectionText != nullptr ? *DirectionText : FText::AsNumber(DegreeValue)
+	);
+
+	DegIndicatorText->SetColorAndOpacity
+	(
+		FSlateColor( DegreeValue == 0 ? m_ZeroDegColor : FLinearColor::White)
+	);
+}
---
+++ Source/Project_Zombie/UI/MainHUD/CompassBarWidget/C_CompassBarWidget.h
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_CompassBarWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ZOMBIE_API UC_CompassBarWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	virtual void NativeOnInitialized() override;
+
+	virtual void NativeConstruct() override;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+private:
+	
+	void SetDisplayDegreeText(float _RotationYaw);
+
+protected:
+
+	// 가운데 현재 바라보는 방위 각도 Indicator TextBlock
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* DegIndicatorText{}; 
+	
+
+protected: // CompassBar 이미지 및 Material 관련
+
+	UPROPERTY(meta = (BindWidget))
+	class UImage* CompassBarImage{};
+	
+private:
+	
+	UMaterialInstanceDynamic* m_CompassBarDynamicMtrl{};
+	
+private:
+	
+	const TMap<int32, FText> m_DegreeToAlphabet = 
+	{
+		{315, 	FText::FromString(TEXT("NW"))},
+		{270, 	FText::FromString(TEXT("W"))},
+		{225, 	FText::FromString(TEXT("SW"))},
+		{180, 	FText::FromString(TEXT("S"))},
+		{135, 	FText::FromString(TEXT("SE"))},
+		{90,	FText::FromString(TEXT("E"))},
+		{45,	FText::FromString(TEXT("NE"))},
+		{0,		FText::FromString(TEXT("N"))},
+	};
+	
+	const FLinearColor m_ZeroDegColor = FLinearColor(FColor(255, 220, 0, 255));
+};
