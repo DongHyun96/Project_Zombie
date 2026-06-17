24d6b07917c2b9e675d18b4e08b739149818e84f
Hhyunseung <105931128+Hhyunseung@users.noreply.github.com>
2026-06-17 12:07:05 +0900

    Input 주석 삭제하고 변수들 추가

--- Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.h
+++ Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.h
@@ -24,6 +24,15 @@ enum class EHandState : uint8
 	WeaponThrowable,
 };
 
+// 시점 상태
+UENUM(BlueprintType)
+enum class EPlayerViewMode : uint8
+{
+	TPS,	// 3인칭
+	FPS,	// 1인칭
+};
+
+
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
@@ -33,6 +42,7 @@ class PROJECT_ZOMBIE_API AC_BasicPlayer : public AC_BasicCharacter
 {
 	GENERATED_BODY()
 	
+// [Component]
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (DisplayName = "SpringArm"))
 	class USpringArmComponent* m_SpringArm;
@@ -44,84 +54,99 @@ protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (DisplayName = "PlayerInput"))
 	class UC_BasicPlayerInputComponent* m_InputComponent;
 
-// 캐릭터 상태 // 이건 나중에 BasicCharacter 로 옮겨도?
+
+// [Status]
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
 	EPlayerState		m_PlayerState;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	bool				m_IsDead;
+
+
+	// => 여기서부터는 나중에 StatComponent으로 분리?
+	// 기본 이동 속도
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
 	float				m_BaseMaxSpeed;
 
-// InputContainer 를 통해서 입력을 받을지...
-// Input
-//protected:
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputMappingContext* DefaultMappingContext;
-//
-//	// 이동			// WASD
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Move;
-//
-//	// 시점 회전		// Mouse
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Look;
-//
-//	// 점프			// SpaceBar
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Jump;
-//
-//	// 공격			// LMB
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Fire;
-//
-//	// 시점 전환		// RMB
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Aim;
-//
-//	// 재장전		// R
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Reload;
-//
-//	// 주무기 1 장착	// 1
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_EquipPrimary1;
-//
-//	// 주무기 2 장착	// 2
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_EquipPrimary2;
-//
-//	// 보조무기 장착	// 3
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_EquipMelee;
-//
-//	// 투척류 무기 장착 // 4
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_EquipThrowable;
-//
-//	// 인벤토리 열기	// Tab
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_ToggleInventory;
-//
-//	// 상호작용		// E
-//	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-//	UInputAction* IA_Interact;
+	// 최대 체력
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float				m_MaxHP;
 
-	// 인칭 전환
-	//UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
-	//UInputAction* IA_ToggleMode;
+	// 현재 체력
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Status")
+	float				m_CurHP;
+
+
+// [Camera]
+protected:
+	// 시점 상태
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	EPlayerViewMode		m_PlayerViewMode;
+
+	// 3인칭 카메라 암 길이
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+	float				m_TPSCameraArmLength;
+
+	// 1인칭 카메라 암 길이
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+	float				m_FPSCameraArmLength;
+
+	// => 나중에 InputComponent으로 분리?
+	// 마우스 감도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Camera")
+	float 				m_MouseSensitivity;
+
+// [Movement]
+protected:
+	// 걷기 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float m_WalkSpeed;
+
+	// 달리기 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float m_RunSpeed;
+
+	// 조준 시 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float m_AimMoveSpeed;
+
+	// 웅크리기 시 속도
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float m_CrouchSpeed;
+
+	/// 우선순위
+	/// 웅크리기 > 조준 > 달리기 > 일반 이동
+	// 달리기 상태
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool m_IsRunning;
+
+	// 조준 상태
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool m_IsAiming;
+
+	// 웅크리기 상태
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool m_IsCrouching;
+
+// [Weapon]
+protected:
+
+
+// [Inventory]
+protected:
+	// => 나중에 InventoryComponent으로 분리?
+	// 상호작용 가능
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	bool m_IsCanInteract;
+
+	// 인벤토리 열려있나
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	bool m_IsInventoryOpen;
 
 
 public:
 	EPlayerState GetPlayerState() { return m_PlayerState; }
-
-// Input Action Functions
-//private:
-//	void InitInput();
-//protected:
-//	void MoveAction(const FInputActionValue& Value);
-//	void LookAction(const FInputActionValue& Value);
-//	void JumpAction();
-//	void FireAction();
 
 
 protected:
