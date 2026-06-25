85e85b0de85a37b7e4c49c7f4fc229e6b8b060f1
shb03008 <sshb03008@gmail.com>
2026-06-25 18:04:22 +0900

    아이템 매니저 생성
    아이템 데이터 테이블 보유

---
+++ Source/Project_Zombie/GameMode/C_ItemMagnager.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameMode/C_ItemMagnager.h"
+
+void UC_ItemMagnager::Initialize(FSubsystemCollectionBase& Collection)
+{
+    Super::Initialize(Collection);
+
+    // 생성자나 에디터 지정이 아닌, 코드에서 데이터 테이블을 static load 합니다.
+    // 경로(Path)는 본인의 데이터 테이블 에디터 경로로 수정하세요.
+    static ConstructorHelpers::FObjectFinder<UDataTable> DataTableFinder(TEXT("/Game/Blueprints/Data/DT_ItemData_Sang.DT_ItemData_Sang"));
+    if (DataTableFinder.Succeeded())
+    {
+        ItemDataTable = DataTableFinder.Object;
+    }
+}
+
+const FItemData* UC_ItemMagnager::GetItemData(FName InRowName) const
+{
+    if (!ItemDataTable || InRowName.IsNone()) return nullptr;
+
+    // FindRow는 내부적으로 TMap 해시 조회를 하기 때문에 매우 빠릅니다.
+    return ItemDataTable->FindRow<FItemData>(InRowName, TEXT("GetItemDataContext"));
+}
