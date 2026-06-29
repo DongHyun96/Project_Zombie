438e0ebefe50721dc897cd1324291fd8fbcb64d2
shb03008 <sshb03008@gmail.com>
2026-06-29 18:58:43 +0900

    InventoryWidget에 On Drop 구현.
    아이템 슬롯이 들어오면 해당 아이템을 뱉어냄.

--- Source/Project_Zombie/UI/InvenUI/C_InventoryWidget.h
+++ Source/Project_Zombie/UI/InvenUI/C_InventoryWidget.h
@@ -6,12 +6,19 @@
 #include "Blueprint/UserWidget.h"
 #include "C_InventoryWidget.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class PROJECT_ZOMBIE_API UC_InventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual bool NativeOnDrop(const FGeometry& InGeometry, const FDragDropEvent& InDragDropEvent, UDragDropOperation* InOperation) override;
+	
+public:
+	class UC_InventoryGridWidget* GetGridWidget() { return GridWidget; }
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	class UC_InventoryGridWidget* GridWidget = nullptr;
 };
