54942ff7bda637ef1502f285a005c1f4091b1a82
shb03008 <sshb03008@gmail.com>
2026-06-29 18:56:16 +0900

    Update C_DragDropOperation.h
    드래그 드롭에서 사용할 오퍼레이션 생성

--- Source/Project_Zombie/UI/InvenUI/DragDropOperation/C_DragDropOperation.h
+++ Source/Project_Zombie/UI/InvenUI/DragDropOperation/C_DragDropOperation.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GlobalData.h"
 #include "Blueprint/DragDropOperation.h"
+#include "UI/InvenUI/C_ItemSlotWidget.h"
 #include "C_DragDropOperation.generated.h"
 
 /**
@@ -13,5 +15,13 @@ UCLASS()
 class PROJECT_ZOMBIE_API UC_DragDropOperation : public UDragDropOperation
 {
 	GENERATED_BODY()
+public:
+	void SetItmeEntry(FInventoryEntry InItemEntry){ItemEntry = InItemEntry;}
 	
+	void SetSlotIndex(int InSlotIndex){SlotIndex = InSlotIndex;}
+	
+	int32 GetSlotIndex(){return SlotIndex;}
+protected:
+	FInventoryEntry ItemEntry;
+	int32 SlotIndex;
 };
