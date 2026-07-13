b432554a2fc2a2eaeed61f01fe81d4e64726d785
DongHyun96 <wadanka09@gmail.com>
2026-07-13 20:23:44 +0900

    StatComponent m_Stats 자료구조 TMap으로 롤백

--- Source/Project_Zombie/Actor/Components/StatComponent/C_StatComponentBase.cpp
+++ Source/Project_Zombie/Actor/Components/StatComponent/C_StatComponentBase.cpp
@@ -19,15 +19,25 @@ void UC_StatComponentBase::OnRegister()
 	// PIE 환경에서 시작 시, OnRegister + BeginPlay 로 인해 InitStat 두 번 호출되는 것 막기 위함 -> 동시에 Blueprint 쪽 내용 업데이트는 안빠지도록 처리
 #if WITH_EDITOR
 	if (!GetWorld() || !GetWorld()->IsGameWorld())
-		InitStat();
+		InitStat(true);
 #endif
+}
+
+#if WITH_EDITOR
+void UC_StatComponentBase::PostLoad()
+{
+	Super::PostLoad();
+
+	if (!GetWorld() || !GetWorld()->IsGameWorld())
+		InitStat(true);
 }
 
 void UC_StatComponentBase::PostEditChangeProperty(FPropertyChangedEvent& _Event)
 {
 	Super::PostEditChangeProperty(_Event);
-	InitStat();
+	InitStat(true);
 }
+#endif
 
 void UC_StatComponentBase::BeginPlay()
 {
@@ -35,12 +45,12 @@ void UC_StatComponentBase::BeginPlay()
 	InitStat();
 }
 
-void UC_StatComponentBase::InitStat()
+void UC_StatComponentBase::InitStat(bool _bModifyForEditor)
 {
 	// 테이블과 행 이름이 설정되어 있어야 한다
 	if (!m_Table || m_RowName.IsNone())
 	{
-		UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table or m_RowName is None!", FColor::Red, 10.f);
+		if (!_bModifyForEditor) UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table or m_RowName is None!", FColor::Red, 10.f);
 		return;
 	}
 
@@ -48,9 +58,12 @@ void UC_StatComponentBase::InitStat()
 	UScriptStruct* ExpectedStruct = GetStatDataStruct();
 	if (!ExpectedStruct || m_Table->GetRowStruct() != ExpectedStruct)
 	{
-		UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table & ExpectedStruct type mismatched!", FColor::Red, 10.f);
+		if (!_bModifyForEditor) UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table & ExpectedStruct type mismatched!", FColor::Red, 10.f);
 		return;
 	}
+
+	if (_bModifyForEditor)
+		Modify();
 
 	// 모든 스탯을 다 지운다
 	m_Stats.Empty();
@@ -58,17 +71,15 @@ void UC_StatComponentBase::InitStat()
 	uint8* RowData = m_Table->FindRowUnchecked(m_RowName);
 	if (!RowData)
 	{
-		UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table->FindRowUnchecked(m_RowName) failed!", FColor::Red, 10.f);
+		if (!_bModifyForEditor) UC_Util::Print("From UC_StatComponentBase::InitStat : m_Table->FindRowUnchecked(m_RowName) failed!", FColor::Red, 10.f);
 		return;
 	}
 	
 	// 데이터를 구성하고 있는 멤버들의 멤버변수명 자체를 키값으로 해서 수치를 기록한다.
-	InitStatFromStruct(FCharacterStatData::StaticStruct(), RowData);
+	InitStatFromStruct(ExpectedStruct, RowData);
 
 	// 추가로 추가할 공용 Stat 추가
 	InitAdditionalStat();
-	
-	Modify(); // 프리뷰 객체의 변경점을 UE Editor에게 알림, UI의 값을 객체의 값으로 재 반영 강제
 }
 
 void UC_StatComponentBase::InitAdditionalStat()
@@ -109,30 +120,23 @@ void UC_StatComponentBase::InitStatFromStruct(UScriptStruct* _InStruct, const vo
 void UC_StatComponentBase::AddStat(const FName& _StatName, float _Amount)
 {
 	// 이미 해당 이름의 스탯이 있으면 추가하지 않는다
-	if (FStatInfo* pInfo = m_Stats.FindByKey(_StatName))
-		return;
-	
-	FStatInfo Info{};
-
-	Info.StatName = _StatName;
-	Info.Value    = _Amount;
-	
-	m_Stats.Add(std::move(Info));
+	if (m_Stats.Contains(_StatName)) return;
+	m_Stats.Add(_StatName, _Amount);
 }
 
 float UC_StatComponentBase::GetStat(const FName& _StatName)
 {
-	if (FStatInfo* pInfo = m_Stats.FindByKey(_StatName))
-		return pInfo->Value;
+	if (float* pStatValue = m_Stats.Find(_StatName))
+		return *pStatValue;
 	
 	return 0.f;
 }
 
 void UC_StatComponentBase::SetStat(const FName& _StatName, float _Value)
 {
-	if (FStatInfo* pInfo = m_Stats.FindByKey(_StatName))
+	if (float* pStatValue = m_Stats.Find(_StatName))
 	{
-		pInfo->Value = _Value;
+		*pStatValue = _Value;
 
 		/*// 체력 Stat 업데이트의 경우 Delegate 호출
 		if (_StatName == TEXT("CurHP"))
--- Source/Project_Zombie/Actor/Components/StatComponent/C_StatComponentBase.h
+++ Source/Project_Zombie/Actor/Components/StatComponent/C_StatComponentBase.h
@@ -7,7 +7,7 @@
 #include "C_StatComponentBase.generated.h"
 
 
-USTRUCT(BlueprintType)
+/*USTRUCT(BlueprintType)
 struct FStatInfo
 {
 	GENERATED_BODY()
@@ -23,13 +23,13 @@ public:
 	/*bool operator==(const FStatInfo& _Other) const
 	{
 		return StatName == _Other.StatName;
-	}*/
+	}#1#
 	
 	bool operator==(const FName& _StatName) const
 	{
 		return StatName == _StatName; 
 	}
-};
+};*/
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Abstract)
 class PROJECT_ZOMBIE_API UC_StatComponentBase : public UActorComponent
@@ -48,8 +48,10 @@ protected:
 
 	// 객체가 메모리에 등록될 때 호출(블루 프린트를 열고 닫을 때, 프리뷰 객체가 생성되는 순간)
 	virtual void OnRegister() override;
-	
+
 #if WITH_EDITOR
+	virtual void PostLoad() override;
+	
 	// 에디터 모드에서만 사용 가능한 가상함수
 	// 에디터 상에서 Preview 객체에 변경점이 발생하면 호출되는 함수
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& _Event) override;
@@ -67,7 +69,7 @@ private:
 	/// 공용으로 사용되는 Stat 항목 추가
 	/// 자식 StatComponent -> 자신의 항목에 맞는 Stat 항목 추가해줄 것
 	/// </summary>
-	void InitStat();
+	void InitStat(bool _bModifyForEditor = false);
 
 	/// <summary>
 	/// 스탯값 가져오기 
@@ -77,7 +79,7 @@ private:
 	/// <summary>
 	/// Init시 사용할 DataTable 형식 return (자식 단에서 무조건 구현 처리해줄 것) 
 	/// </summary>
-	virtual UScriptStruct* GetStatDataStruct() const PURE_VIRTUAL(UC_StatComponentBase::GetStatDataStruct, return nullptr;)
+	virtual UScriptStruct* GetStatDataStruct() const PURE_VIRTUAL(UC_StatComponentBase::GetStatDataStruct, return nullptr;);
 	
 	/// <summary>
 	/// 자식 StatComponent쪽에서 더 추가할 Stat 내용이 있다면 해당 함수 override하여 추가할 것
@@ -94,9 +96,9 @@ protected:
 	FName m_RowName{};
 
 	// 보유 스탯 (MaxStat, CurStat 모두 포함)
-	// UPROPERTY(ReplicatedUsing = OnRep, VisibleAnywhere, BlueprintReadOnly, Category = "Stat")
+	// UPROPERTY(ReplicatedUsing = OnRep, VisibleAnywhere, BlueprintReadOnly, Category = "Stat") -> 사용 불가
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Stat")
-	TArray<FStatInfo> m_Stats{};
+	TMap<FName, float> m_Stats{};
 
 private:
 	
