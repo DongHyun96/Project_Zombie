07f94e03e3723de008c7180b323d5d76bbbc8dc6
HR0301 <gkfkdwkd@naver.com>
2026-07-13 22:00:46 +0900

    발사테스트 ?

--- Source/Project_Zombie/Actor/Character/NPC/Enemy/Zombie/Skill/ToxicAttack/C_ToxicAttack.cpp
+++ Source/Project_Zombie/Actor/Character/NPC/Enemy/Zombie/Skill/ToxicAttack/C_ToxicAttack.cpp
@@ -6,6 +6,8 @@
 #include "Actor/Character/NPC/Enemy/C_BasicEnemy.h"
 #include "Actor/Character/NPC/Enemy/Zombie/Skill/C_EnemySkillData.h"
 
+#include "Actor/Character/NPC/Enemy/Zombie/Skill/Projectile/C_ToxicProjectile.h"
+
 UC_ToxicAttack::UC_ToxicAttack()
 {
 }
@@ -19,5 +21,26 @@ void UC_ToxicAttack::Activate(AC_BasicEnemy* _Owner, UC_EnemySkillData* _Data)
 
 void UC_ToxicAttack::Fire(AC_BasicEnemy* _Owner, UC_EnemySkillData* _Data)
 {
+	//UE_LOG(LogTemp, Warning, TEXT("Fire"));
+
+	if (!_Owner || !_Data)
+		return;
+
+	if (!_Data->ProjectileClass)
+		return;
+
+	FVector SpawnLocation = _Owner->GetActorLocation();
+	FRotator SpawnRotation = _Owner->GetActorRotation();
+
+	AC_ToxicProjectile* Projectile =
+		GetWorld()->SpawnActor<AC_ToxicProjectile>(
+			_Data->ProjectileClass,
+			SpawnLocation,
+			SpawnRotation);
+
+	if (Projectile)
+	{
+		Projectile->InitProjectile(_Owner, _Data);
+	}
 }
 
