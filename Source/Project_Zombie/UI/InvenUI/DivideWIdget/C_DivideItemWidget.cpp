99223e6322d2c40402848b21c8cac726134a9dfb
shb03008 <sshb03008@gmail.com>
2026-07-16 17:19:03 +0900

    아이템 나누기 위젯 정리

--- Source/Project_Zombie/UI/InvenUI/DivideWIdget/C_DivideItemWidget.cpp
+++ Source/Project_Zombie/UI/InvenUI/DivideWIdget/C_DivideItemWidget.cpp
@@ -3,13 +3,10 @@
 
 #include "UI/InvenUI/DivideWIdget/C_DivideItemWidget.h"
 
-#include "DelayAction.h"
 #include "Components/EditableText.h"
 #include "Components/Image.h"
 #include "Components/Slider.h"
-#include "Components/TextBlock.h"
 #include "GameModeAndManager/C_ItemManager.h"
-#include "Kismet/KismetSystemLibrary.h"
 #include "Utility/C_Util.h"
 
 void UC_DivideItemWidget::UpdateWidget()
@@ -28,31 +25,32 @@ void UC_DivideItemWidget::UpdateWidget()
 	
 	CalculateDivideCount(FMath::TruncToInt32(MaxCount * 0.5f));
 	
-	CountSlider->SetMinValue(1.0f);
 	CountSlider->SetMaxValue(MaxCount);
-	MaxItemCountText->SetText(FText::FromString(FString::FromInt(MaxCount)));
-	UpdateDroppedCount();
-	UpdateSlider();
 	
+	MaxItemCountText->SetText(FText::FromString(FString::FromInt(MaxCount)));
+	
+	ItemIcon->SetBrushFromTexture(curDraggedItemData->IconTexture.Get());
+	
+	UpdateDroppedCount();
+	
+	UpdateSlider();
 	
 	DroppedCountText->SetSelectAllTextWhenFocused(true);
 	
-	DroppedCountText->SetFocus();
+	DroppedCountText->SetKeyboardFocus();
+	
 	SetVisibility(ESlateVisibility::Visible);
 }
 
 void UC_DivideItemWidget::CalculateDivideCount(int32 InCurCount)
 {
-	UC_Util::Print(InCurCount,FColor::Blue, 10.f);
-	
 	if (InCurCount <= 0)
 	{
 		DividedCount = 1;
-		//FocusAndSelectAllText();
-		UC_Util::Print(DividedCount,FColor::Black, 10.f);
-		UC_Util::Print("Calculate",FColor::Black, 10.f);
 		
-		CountSlider->SetFocus();
+		CountSlider->SetKeyboardFocus();
+		
+		// 위에서 포커스를 잠시 다른데 주고 다음 틱에 DroppedCountText가 포커스를 갖도록 해서 SelectAll 하도록 함.
 		GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UC_DivideItemWidget::ReFocusDroppedCountText);
 	}
 	else if (InCurCount > CursorItem.Item.CurCount)
@@ -67,25 +65,17 @@ void UC_DivideItemWidget::CalculateDivideCount(int32 InCurCount)
 
 void UC_DivideItemWidget::UpdateSlider()
 {
-	//if (CountSlider->GetValue() == DividedCount) return;
-	UC_Util::Print(DividedCount,FColor::Red, 10.f);
 	CountSlider->SetValue(static_cast<float>(DividedCount));
-	//CountSlider->SetFocus();
 }
 
 void UC_DivideItemWidget::UpdateDroppedCount()
 {
-	//if (DroppedCountText->GetText().ToString() == FString::FromInt(DividedCount)) return; 
-	UC_Util::Print(DividedCount,FColor::Emerald, 10.f);
 	DroppedCountText->SetText(FText::FromString(FString::FromInt(DividedCount)));
-	//DroppedCountText->SetSelectAllTextWhenFocused(false);
-	//DroppedCountText->SetFocus();
 }
 
 void UC_DivideItemWidget::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
-	UC_Util::Print(DividedCount,FColor::Magenta, 10.f);
 	CountSlider->OnValueChanged.AddUniqueDynamic(
 	this,
 	&UC_DivideItemWidget::HandleOnValueChangedCountSlider);
@@ -97,10 +87,10 @@ void UC_DivideItemWidget::NativeOnInitialized()
 
 void UC_DivideItemWidget::HandleOnValueChangedCountSlider(float InValue)
 {
-	UC_Util::Print(DividedCount,FColor::Red, 10.f);
 	DividedCount = FMath::TruncToInt32(InValue);
-	//if (CountSlider->GetValue() == DividedCount) return;
+	
 	UpdateSlider();
+	
 	UpdateDroppedCount();
 }
 
@@ -108,53 +98,20 @@ void UC_DivideItemWidget::HandleOnTextChangedDroppedCount(const FText& InText)
 {
 	FString TextString = InText.ToString();
     
-	// [수정됨] 텍스트가 완전히 비어있다면 0을 넘겨서 'CalculateDivideCount'가 0 이하 예외 처리를 하도록 유도합니다.
-	//int32 TargetValue = TextString.IsEmpty() ? 0 : FCString::Atoi(*TextString);
-	UC_Util::Print(TextString,FColor::Red, 10.f);
-	if (TextString == FString(""))
-	{
-		UC_Util::Print("Text Is Null!",FColor::Red, 10.f);
-	}
-	//if (TextString == FString(""))
-	//{
-	//	DividedCount = 1;
-	//	
-	//	UpdateDroppedCount();
-	//	
-	//	DroppedCountText->SetFocus();
-	//	
-	//	return;
-	//}
-	
 	CalculateDivideCount(FCString::Atoi(*TextString));
 	
-	//if (DroppedCountText->GetText().ToString() == FString::FromInt(DividedCount)) return; 
-	
 	UpdateSlider();
+	
 	UpdateDroppedCount();
-}
-
-void UC_DivideItemWidget::CustomDelay(UObject* WorldContextObject, float Duration, FLatentActionInfo LatentInfo)
-{
-	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
-	{
-		FLatentActionManager& LatentActionManager = World->GetLatentActionManager();
-        
-		// 언리얼 엔진 내부의 Delay 액션을 직접 등록해 실행합니다.
-		LatentActionManager.AddNewAction(
-			LatentInfo.CallbackTarget, 
-			LatentInfo.UUID, 
-			new FDelayAction(Duration, LatentInfo)
-		);
-	}
 }
 
 void UC_DivideItemWidget::ReFocusDroppedCountText()
 {
-	DroppedCountText->SetFocus();
-	UC_Util::Print("ReFocus",FColor::Red, 10.f);
+	if (DroppedCountText->HasKeyboardFocus())
+	{
+		GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UC_DivideItemWidget::ReFocusDroppedCountText);
+		return;
+	}
+	
+	DroppedCountText->SetKeyboardFocus();
 }
-
-//void UC_DivideItemWidget::HandleOnClickedExitButton()
-//{
-//}
--- Source/Project_Zombie/UI/InvenUI/DivideWIdget/C_DivideItemWidget.h
+++ Source/Project_Zombie/UI/InvenUI/DivideWIdget/C_DivideItemWidget.h
@@ -25,6 +25,7 @@ public:
 public:
 	void SetCursorItem(const FCursorItem& InCursorItem) { CursorItem = InCursorItem; }
 	
+	// 몇개로 나눌지 입력이 들어왔을 때 최대 최소 사이에서 DividedCount가 정의 되도록함.
 	UFUNCTION(BlueprintCallable)
 	void CalculateDivideCount(int32 InCurCount);
 	
@@ -45,38 +46,35 @@ protected:
 	UFUNCTION()
 	void HandleOnTextChangedDroppedCount(const FText& InText);
 	
-	UFUNCTION(BlueprintCallable, Category = "CustomDelay", meta = (Latent, LatentInfo = "LatentInfo", WorldContext = "WorldContextObject"))
-	void CustomDelay(UObject* WorldContextObject, float Duration, FLatentActionInfo LatentInfo);
-	
-	UFUNCTION(BlueprintImplementableEvent)
-	void FocusAndSelectAllText();
-	
-	
 protected:
 	// 딜레이 처리를 위한 타이머 핸들
-	//FTimerHandle FocusTimerHandle;
 	UFUNCTION()
 	void ReFocusDroppedCountText();
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	UImage* ItemIcon{};
 	
+	// 아이템 이름 Text Block
 	UPROPERTY(EditAnywhere, BlueprintReadWrite,meta = (BindWidget))
 	UTextBlock* ItemNameTextBlock{};
 	
+	// 나눌 아이템의 갯수 Text Block
 	UPROPERTY(EditAnywhere, BlueprintReadWrite,meta = (BindWidget))
 	UEditableText* DroppedCountText{};
 	
+	// 드래그되어 나누기 시도를 당하고 있는 아이템의 갯수를 보여주는 Text Block
 	UPROPERTY(EditAnywhere, BlueprintReadWrite,meta = (BindWidget))
 	UEditableText* MaxItemCountText{};
 
+	// 몇개로 나눌지 시각적으로 보고, 정할 수 있는 슬라이더 바
 	UPROPERTY(EditAnywhere, BlueprintReadWrite,meta = (BindWidget))
 	USlider* CountSlider{};
 	
+	// 현재 드래그된 아이템
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FCursorItem CursorItem{};
 	
-	
+	// 몇개로 나눌지 알려주는 값.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 DividedCount = 0;
 };
