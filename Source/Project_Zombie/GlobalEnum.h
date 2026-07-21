323efd4be11527937d5fba71aa755027e7b1b5f4
shb03008 <sshb03008@gmail.com>
2026-07-21 20:55:30 +0900

    아이템 데이터 테이블에 접근할 때 사용할 아이템 타입 enum추가

--- Source/Project_Zombie/GlobalEnum.h
+++ Source/Project_Zombie/GlobalEnum.h
@@ -6,8 +6,8 @@ enum class EItemType : uint8
 {
     MAINWEAPON,
     MELEEWEAPON,
-    GADGET,
     THROWABLE,
+    GADGET,
     CONSUMABLE,
     MATTER,
     
@@ -27,4 +27,14 @@ enum class EWeaponType : uint8
     Gun         UMETA(DisplayName = "Gun"),
     Melee       UMETA(DisplayName = "Melee"),
     Throwable   UMETA(DisplayName = "Throwable")
+};
+
+// 아이템 데이터 테이블에 접근할 때 사용할 아이템 타입 enum
+UENUM(BlueprintType)
+enum class EItemTableType : uint8
+{
+    General,
+    Gun,
+    Melee,
+    Throwable
 };
