af64f4afde2fa60bdd22b177ad00d027b02990a4
shb03008 <sshb03008@gmail.com>
2026-07-31 01:50:58 +0900

    아이템 강호 구현

--- Source/Project_Zombie/Actor/Components/UpgradeComponent/C_ItemUpgradeComponent.cpp
+++ Source/Project_Zombie/Actor/Components/UpgradeComponent/C_ItemUpgradeComponent.cpp
@@ -2,13 +2,19 @@
 
 
 #include "Actor/Components/UpgradeComponent/C_ItemUpgradeComponent.h"
+#include "Actor/Character/Player/C_BasicPlayer.h"
+#include "Actor/Components/C_InvenComponent.h"
+#include "Actor/Components/C_EquippedComponent.h"
+#include "Actor/ItemActor/Weapon/C_WeaponBase.h"
+#include "GameModeAndManager/C_ItemManager.h"
+#include "GameModeAndManager/C_UIManager.h"
 
 // Sets default values for this component's properties
 UC_ItemUpgradeComponent::UC_ItemUpgradeComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -31,4 +37,50 @@ void UC_ItemUpgradeComponent::TickComponent(float DeltaTime, ELevelTick TickType
 
 	// ...
 }
+
+void UC_ItemUpgradeComponent::UpgradeItem(AC_BasicPlayer* InPlayer, int32 InItemIndex, EUpgradableStats TargetStat)
+{
+	UC_InvenComponent* InvenComp = InPlayer->GetInvenComponent();
+
+	FInventoryEntry* Entry = InvenComp->GetSlotDataPtr(InItemIndex);
+
+	PRINT_LOCAL(GetWorld(), "UpgradeItem", FColor::Blue, 5.f);
+
+	if (!Entry->HasEquipmentData()) return;
+
+	//Entry.CustomData.GetPtr
+
+	FEquipmentCustomData* EquipmentData = Entry->GetEquipmentDataPtr();
+
+	if (!EquipmentData) return;
+
+	EquipmentData->AddStatGrade(TargetStat, 1);
+
+	InvenComp->MarkSlotDirty(InItemIndex);
+
+	if (InItemIndex < static_cast<int32>(EWeaponSlot::None))
+	{
+		UC_EquippedComponent* EquipComp = InPlayer->GetEquippedComponent();
+
+		AC_WeaponBase* CurWeapon = EquipComp->GetSlotWeapon(static_cast<EWeaponSlot>(InItemIndex));
+
+		UC_ItemManager* ItemManager = InPlayer->GetGameInstance()->GetSubsystem<UC_ItemManager>();
+
+		if (!ItemManager) return;
+
+		const FWeaponData* WeaponData = nullptr;
+
+		WeaponData = ItemManager->GetWeaponData(Entry->ItemRowName);
+
+		if (WeaponData)
+		{
+			// TODO : 서버에서 이 아이템의 정보를 알 필요 없다면 안해도 될 수 도?
+			CurWeapon->InitializeItemData(WeaponData);
+			// TODO : 클라에서 InitializeItemData함수를 호출해서 클라가 자신의 강화를 착용중인 아이템에 적용 해야 하는데.
+			// 클라에서 InitializeItemData를 호출시키는 함수를 하나 만들거나, 강제로 무기를 재장착 하는 방법이 있음.
+			EquipComp->Client_UpdateWeaponData(static_cast<EWeaponSlot>(InItemIndex), Entry->ItemRowName);
+		}
+	}
+}
+
 
--- Source/Project_Zombie/Actor/Components/UpgradeComponent/C_ItemUpgradeComponent.h
+++ Source/Project_Zombie/Actor/Components/UpgradeComponent/C_ItemUpgradeComponent.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "GlobalData.h"
 #include "C_ItemUpgradeComponent.generated.h"
 
+class AC_BasicPlayer;
 
 // 아이템을 강화하는 컴포넌트
 // InteractableBase쪽에 붙어서 UpgradeWidget을 통해 클라가 서버에 강화를 요청하도록 할 예정.
@@ -26,5 +28,5 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	virtual void UpgradeItem(AC_BasicPlayer* InPlayer, int32 InItemIndex, EUpgradableStats TargetStat);
 };
