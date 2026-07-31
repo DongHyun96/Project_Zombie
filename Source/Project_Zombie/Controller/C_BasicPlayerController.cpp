b58e0eba769cf4be6aa448e620ffdf6416d295bd
shb03008 <sshb03008@gmail.com>
2026-07-31 19:10:01 +0900

    PlayerController에 멀티 관련 기능 추가

--- Source/Project_Zombie/Controller/C_BasicPlayerController.cpp
+++ Source/Project_Zombie/Controller/C_BasicPlayerController.cpp
@@ -5,7 +5,42 @@
 
 #include "Actor/Components/C_InvenComponent.h"
 #include "GameFramework/PlayerState.h"
+#include "GameModeAndManager/C_UIManager.h"
+#include "Net/UnrealNetwork.h"
+#include "UI/InvenUI/C_InventoryWidget.h"
+#include "UI/InvenUI/Upgrade/C_ItemUpgradeWidget.h"
 
+
+void AC_BasicPlayerController::FinishItemUpgrade()
+{
+	AC_UIManager* UIManager = AC_UIManager::Get(GetWorld());
+	
+	if (!UIManager) return;
+	
+	UC_InventoryWidget* InvenWidget = UIManager->GetInventoryWidget();
+	
+	if (!InvenWidget) return;
+	
+	UC_ItemUpgradeWidget* ItemUpgradeWidget = InvenWidget->GetItemUpgradeWidget();
+	
+	if (!ItemUpgradeWidget) return;
+	
+	ItemUpgradeWidget->SetIsUpgrading(false);
+	
+	//ItemUpgradeWidget->UpdateWidget();
+	
+	PRINT_LOCAL(GetWorld(), "FinishItemUpgrade", FColor::Red, 5.f);
+}
+
+void AC_BasicPlayerController::OnRep_IsUpgrading()
+{
+	FinishItemUpgrade();
+}
+
+void AC_BasicPlayerController::Client_FinishItemUpgrade_Implementation()
+{
+	FinishItemUpgrade();
+}
 
 void AC_BasicPlayerController::Destroyed()
 {
@@ -27,4 +62,11 @@ void AC_BasicPlayerController::Destroyed()
 	}
 
 	Super::Destroyed();
+}
+
+void AC_BasicPlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(AC_BasicPlayerController, bIsUpgrading);
 }
