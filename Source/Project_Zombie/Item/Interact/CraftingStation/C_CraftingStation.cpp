f8c224eaa702524bf5aa52b7de56dfadf002042a
shb03008 <sshb03008@gmail.com>
2026-08-03 01:36:28 +0900

    상호작용 객체 세분화 자식 클래스 만듬

---
+++ Source/Project_Zombie/Item/Interact/CraftingStation/C_CraftingStation.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/Interact/CraftingStation/C_CraftingStation.h"
+
---
+++ Source/Project_Zombie/Item/Interact/CraftingStation/C_CraftingStation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/Interact/C_InteractableBase.h"
+#include "C_CraftingStation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ZOMBIE_API AC_CraftingStation : public AC_InteractableBase
+{
+	GENERATED_BODY()
+	
+};
