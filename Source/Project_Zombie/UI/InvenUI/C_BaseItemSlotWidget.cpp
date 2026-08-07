5e45daf89bcb2bb003e7aea814a6df0735fb6da7
shb03008 <sshb03008@gmail.com>
2026-08-07 19:05:00 +0900

    Print제거

--- Source/Project_Zombie/UI/InvenUI/C_BaseItemSlotWidget.cpp
+++ Source/Project_Zombie/UI/InvenUI/C_BaseItemSlotWidget.cpp
@@ -61,21 +61,15 @@ FReply UC_BaseItemSlotWidget::NativeOnMouseButtonDown(const FGeometry& InGeometr
 
 void UC_BaseItemSlotWidget::NativeOnDragDetected(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent, UDragDropOperation*& OutOperation)
 {
-    PRINT_LOCAL(GetWorld(), "Try AssociatedInvenComp", FColor::Red, 5.f);
-    
     if (!AssociatedInvenComp) return;
 
     AC_BasicPlayer* Owner = Cast<AC_BasicPlayer>(GetOwningPlayerPawn());
-    PRINT_LOCAL(GetWorld(), "Try Owner", FColor::Red, 5.f);
     if (!Owner) return;
     
     const TArray<FInventoryEntry>& ItemArr = AssociatedInvenComp->GetInventoryItems();
-    PRINT_LOCAL(GetWorld(), "Try IsValidIndex", FColor::Red, 5.f);
     
     FString Msg = ItemArr[curSlotIdx].ItemRowName.ToString();
-    PRINT_LOCAL(GetWorld(), Msg, FColor::Red, 5.f);
     if (!ItemArr.IsValidIndex(curSlotIdx) || ItemArr[curSlotIdx].ItemRowName == NAME_None) return;
-
 
     FInventoryEntry entry = ItemArr[curSlotIdx];
     
@@ -83,9 +77,7 @@ void UC_BaseItemSlotWidget::NativeOnDragDetected(const FGeometry& InGeometry, co
     
     Owner->Server_RequestDragItemSlot(curSlotIdx, AssociatedInvenComp);
     
-    PRINT_LOCAL(GetWorld(), "Try SetCurDraggedItem", FColor::Red, 5.f);
     if (!Owner->SetCurDraggedItem(entry, AssociatedInvenComp, curSlotIdx)) return;
-    PRINT_LOCAL(GetWorld(), "Success SetCurDraggedItem", FColor::Red, 5.f);
 
     UC_DragDropOperation* DragOperation = NewObject<UC_DragDropOperation>();
     InitDragVisual(DragOperation);
