ba0be9cba574c48dcb20efefa9167c31973be3e4
shb03008 <sshb03008@gmail.com>
2026-08-09 02:04:29 +0900

    Update C_BasicPlayer.cpp

--- Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.cpp
+++ Source/Project_Zombie/Actor/Character/Player/C_BasicPlayer.cpp
@@ -80,13 +80,12 @@ void AC_BasicPlayer::PossessedBy(AController* NewController)
 		{
 			if (UC_InvenComponent* InvenComp = FindComponentByClass<UC_InvenComponent>())
 			{
-				
-				
 				InvenComp->LoadInventoryFromBackup(PS->GetSavedInventory());
 			}
 			
 			if (m_EquippedComponent && HasAuthority())
 			{
+				// 서버는 이 시점에 EquippedComponent의 OwnerPlayer가 없음.(비긴에서 넣어주고 있음)
 				m_EquippedComponent->SetOwnerPlayer(this);
 				
 				for (int32 i = 0 ; i < static_cast<int32>(EWeaponSlot::None) ; ++i)
