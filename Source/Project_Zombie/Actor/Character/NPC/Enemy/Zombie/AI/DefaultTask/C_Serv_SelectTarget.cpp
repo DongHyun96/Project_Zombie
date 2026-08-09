5440aa7c47855cdd7fb59ffe5eec36082af66720
HR0301 <gkfkdwkd@naver.com>
2026-08-10 04:31:57 +0900

    추적소리 수정 및 타워에도 데미지 입힐시 소리재생

--- Source/Project_Zombie/Actor/Character/NPC/Enemy/Zombie/AI/DefaultTask/C_Serv_SelectTarget.cpp
+++ Source/Project_Zombie/Actor/Character/NPC/Enemy/Zombie/AI/DefaultTask/C_Serv_SelectTarget.cpp
@@ -37,11 +37,11 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 
 	UBlackboardComponent* pBBCom = _OwnCom.GetBlackboardComponent();
 	if (!pBBCom) return;
-	
+
 	// 인지범위를 벗어난 대상이 일정시간이 지나면 인지목록에서 제거
 	pController->ClearSensedTarget(3.f);
 
-	float MaxAggro      = -1.f;
+	float MaxAggro = -1.f;
 	AActor* pBestTarget = nullptr;
 	FVector Pos{};
 
@@ -49,7 +49,7 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 	for (const FSensedTargetInfo& Info : pController->GetSensedTargets())
 	{
 		if (!Info.Target.IsValid()) continue;
-		
+
 		// 감지된 PointTower인 경우에, 현재 공격 가능한 상황인지 따짐 -> 공격 불가능한 타워의 경우 넘어감
 		if (AC_PointTower* PointTower = Cast<AC_PointTower>(Info.Target))
 			if (!PointTower->CanCurrentlyAttackedByZombie()) continue;
@@ -57,25 +57,25 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 		// 어그로 수치 먼저 판단
 		if (MaxAggro < Info.AggroValue)
 		{
-			MaxAggro    = Info.AggroValue;
+			MaxAggro = Info.AggroValue;
 			pBestTarget = Info.Target.Get();
-			Pos         = Info.Target->GetActorLocation();
+			Pos = Info.Target->GetActorLocation();
 		}
 
 		// 어그로가 동일한 경우
 		else if (MaxAggro == Info.AggroValue)
 		{
 			const float DistSqrOrigin = FVector::DistSquared(pZombie->GetActorLocation(), Pos);
-			const float DistSqrNew    = FVector::DistSquared(pZombie->GetActorLocation(), Info.Target->GetActorLocation());
+			const float DistSqrNew = FVector::DistSquared(pZombie->GetActorLocation(), Info.Target->GetActorLocation());
 
 			if (DistSqrNew < DistSqrOrigin)
 			{
 				pBestTarget = Info.Target.Get();
-				Pos         = Info.Target->GetActorLocation();
+				Pos = Info.Target->GetActorLocation();
 			}
 		}
 	}
-	
+
 	if (pBestTarget) // BestTarget가 나온 상황
 	{
 		// BestTarget을 Target으로 지정
@@ -88,18 +88,10 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 		{
 			pZombie->StartChaseSoundLoop();
 		}
-	
+
 		pBBCom->SetValueAsObject(m_Target.SelectedKeyName, pBestTarget);
 		return;
 	}
-
-	// 여기까지 왔다는 것은 현재 추격할 Target을 찾지 못한 상황
-
-	// 추격음 반복 정지
-	pZombie->StopChaseSoundLoop();
-
-	// 기존 Blackboard Target도 제거
-	pBBCom->ClearValue(m_Target.SelectedKeyName);
 
 	// BestTarget이 나오지 않은 상황
 	//  -> 가장 가까운 플레이어 또는 거점을 찾는다
@@ -110,7 +102,7 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 		m_GameLevelManager = GetWorld()->GetSubsystem<UC_GameLevelManager>();
 		if (!m_GameLevelManager) return;
 	}
-	
+
 	for (AC_BasicPlayer* pPlayer : m_GameLevelManager->GetPlayers())
 	{
 		if (!pPlayer) continue;
@@ -119,7 +111,7 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 
 		if (DistSqr < MinDistSqr)
 		{
-			MinDistSqr  = DistSqr;
+			MinDistSqr = DistSqr;
 			pBestTarget = pPlayer;
 		}
 	}
@@ -131,11 +123,27 @@ void UC_Serv_SelectTarget::TickNode(UBehaviorTreeComponent& _OwnCom, uint8* _Nod
 
 		if (DistSqr < MinDistSqr)
 		{
-			MinDistSqr  = DistSqr;
+			MinDistSqr = DistSqr;
 			pBestTarget = PointTower;
 		}
 	}
 
 	// 가장 가까운 타겟을 블랙보드에 타겟으로 설정
-	pBBCom->SetValueAsObject(m_Target.SelectedKeyName, pBestTarget);
+	UObject* CurTarget = pBBCom->GetValueAsObject(m_Target.SelectedKeyName);
+
+	if (IsValid(pBestTarget))
+	{
+		if (!IsValid(CurTarget))
+		{
+			pZombie->StartChaseSoundLoop();
+		}
+
+		pBBCom->SetValueAsObject(m_Target.SelectedKeyName, pBestTarget);
+
+		return;
+	}
+
+	pZombie->StopChaseSoundLoop();
+
+	pBBCom->ClearValue(m_Target.SelectedKeyName);
 }
