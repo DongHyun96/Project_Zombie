f8bec7cd54f2c3a42dc6a953cb3d10b9a4cca517
DongHyun96 <50581769+DongHyun96@users.noreply.github.com>
2026-08-10 10:21:31 +0900

    OtherPlayerStatWidget HP 업데이트 바인딩 재시도

--- Source/Project_Zombie/Actor/Components/C_PlayerStatComponent.cpp
+++ Source/Project_Zombie/Actor/Components/C_PlayerStatComponent.cpp
@@ -44,8 +44,11 @@ void UC_PlayerStatComponent::BeginPlay()
 			if (StatWidget)
 				this->OnCurHPUpdatedDelegate.AddUObject(StatWidget, &UC_PlayerStatWidget::UpdateHPBarRatio);
 		});
+	}*/
+	if (!m_OwnerCharacter->IsLocallyControlled())
+	{
+		OnCurHPUpdatedDelegate.AddUObject(this, &UC_PlayerStatComponent::UpdateOtherPlayerHPBar);
 	}
-	else OnCurHPUpdatedDelegate.AddUObject(this, &UC_PlayerStatComponent::UpdateOtherPlayerHPBar);*/
 }
 
 void UC_PlayerStatComponent::Server_RequestStatUpgrade(AC_StatUpgradeStation* InInteractableActor,
