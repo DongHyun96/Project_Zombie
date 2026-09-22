4aa45a26c53cab7ad85085320920d76682e5f284
DongHyun96 <50581769+DongHyun96@users.noreply.github.com>
2026-09-22 16:32:00 +0900

    Shipping 관련 디버깅 (Shipping시 Editor용 모듈 빌드에서 제거)

--- Source/Project_Zombie.Target.cs
+++ Source/Project_Zombie.Target.cs
@@ -11,11 +11,5 @@ public class Project_ZombieTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Project_Zombie");
-		RegisterModulesCreatedByRider();
-	}
-
-	private void RegisterModulesCreatedByRider()
-	{
-		ExtraModuleNames.AddRange(new string[] { "Project_ZombieEditor" });
 	}
 }
